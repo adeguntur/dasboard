6ed5312ed345b37f719ed05bd8ff044dcf965550
ihalotec <ihalotec@gmail.com>
2020-05-31 04:10:51 +0700

    onclick test

--- public/js/demo_1/dashboard.js
+++ public/js/demo_1/dashboard.js
@@ -21,18 +21,72 @@
         datasets: [
           {
             label: "Thousands (RP)",
-            backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
+            borderWidth: 1,
+            backgroundColor: ["#878787", "#ffa07a", "#ffda00", "#00ff5f", "#ff007f"],
             data: data.jml_importnegara,
           }
           ]
           },
-          options: {
-            legend: { display: false },
-            title: {
-              display: true,
-              
-            }
-          }
+         options: {
+           responsive: true,
+           legend: {
+             display: false
+           },
+           title: {
+             display: true,
+           },
+           scales: {
+             yAxes: [{
+               ticks: {
+                 beginAtZero: true
+               }
+             }],
+             xAxes: [{
+               gridLines: {
+                 display: false
+               },
+               ticks: {
+                 beginAtZero: true
+               }
+             }]
+           },
+           "hover": {
+             "animationDuration": 0
+           },
+           onClick: function (event, array) {
+             let element = this.getElementAtEvent(event);
+             if (element.length > 0) {
+               var series = element[0]._model.datasetLabel;
+               var label = element[0]._model.label;
+               var value = this.data.datasets[element[0]._datasetIndex].data[element[0]._index];
+               alert(label + " dengan nilai " + value);
+             }
+           },
+           "animation": {
+             "duration": 1,
+             "onComplete": function () {
+               var chartInstance = this.chart,
+                 ctx = chartInstance.ctx;
+
+               ctx.font = Chart.helpers.fontString(Chart.defaults.global.defaultFontSize, Chart.defaults.global.defaultFontStyle, Chart.defaults.global.defaultFontFamily);
+               ctx.textAlign = 'center';
+               ctx.textBaseline = 'bottom';
+
+               this.data.datasets.forEach(function (dataset, i) {
+                 var meta = chartInstance.controller.getDatasetMeta(i);
+
+                 meta.data.forEach(function (bar, index) {
+                   var data = dataset.data[index];
+                   ctx.fillText(data, bar._model.x, bar._model.y + 20);
+
+                 });
+
+               });
+
+             }
+
+           }
+         }
         })
         
         //5 Pelabuhan pemasukan terbesar
@@ -44,17 +98,69 @@
           datasets: [
           {
             label: "Thousands (RP)",
-            backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
+            borderWidth: 1,
+            backgroundColor: ["#878787", "#ff007f","#964b00","#ffda00","#ff7b00"],
             data: data.jml_pib
           }
         ]
           },
           options: {
+            responsive: true,
             legend: { display: false },
             title: {
               display: true,
-              
-            }
+            },
+             scales: {
+               yAxes: [{
+                 ticks: {
+                   beginAtZero: true
+                 }
+               }],
+               xAxes: [{
+                 gridLines: {
+                   display: false
+                 },
+                 ticks: {
+                   beginAtZero: true
+                 }
+               }]
+             },
+             "hover": {
+               "animationDuration": 0
+             },
+             onClick: function (event, array) {
+               let element = this.getElementAtEvent(event);
+               if (element.length > 0) {
+                 var series = element[0]._model.datasetLabel;
+                 var label = element[0]._model.label;
+                 var value = this.data.datasets[element[0]._datasetIndex].data[element[0]._index];
+                 alert(label + " dengan nilai " + value);
+               }
+             },
+             "animation": {
+               "duration": 1,
+               "onComplete": function () {
+                 var chartInstance = this.chart,
+                   ctx = chartInstance.ctx;
+
+                 ctx.font = Chart.helpers.fontString(Chart.defaults.global.defaultFontSize, Chart.defaults.global.defaultFontStyle, Chart.defaults.global.defaultFontFamily);
+                 ctx.textAlign = 'center';
+                 ctx.textBaseline = 'bottom';
+
+                 this.data.datasets.forEach(function (dataset, i) {
+                   var meta = chartInstance.controller.getDatasetMeta(i);
+
+                   meta.data.forEach(function (bar, index) {
+                     var data = dataset.data[index];
+                     ctx.fillText(data, bar._model.x, bar._model.y + 20);
+
+                   });
+
+                 });
+
+               }
+
+             }
           }
         })
       
@@ -66,18 +172,70 @@
              labels: data.nama_importir,
              datasets: [{
                label: "Thousands (RP)",
-               backgroundColor: ["#3e95cd", "#8e5ea2", "#3cba9f", "#e8c3b9", "#c45850"],
+               borderWidth: 1,
+               backgroundColor: "#878787",
                data: data.total_importir
              }]
            },
            options: {
+             responsive: true,
              legend: {
                display: false
              },
              title: {
                display: true,
+             },
+              scales: {
+                yAxes: [{
+                  ticks: {
+                    beginAtZero: true
+                  }
+                }],
+                xAxes: [{
+                  gridLines: {
+                    display: false
+                  },
+                  ticks: {
+                    beginAtZero: true
+                  }
+                }]
+              },
+              "hover": {
+                "animationDuration": 0
+              },
+              onClick: function (event, array) {
+                let element = this.getElementAtEvent(event);
+                if (element.length > 0) {
+                  var series = element[0]._model.datasetLabel;
+                  var label = element[0]._model.label;
+                  var value = this.data.datasets[element[0]._datasetIndex].data[element[0]._index];
+                  alert(label + " dengan nilai " + value);
+                }
+              },
+              "animation": {
+                "duration": 1,
+                "onComplete": function () {
+                  var chartInstance = this.chart,
+                    ctx = chartInstance.ctx;
 
-             }
+                  ctx.font = Chart.helpers.fontString(Chart.defaults.global.defaultFontSize, Chart.defaults.global.defaultFontStyle, Chart.defaults.global.defaultFontFamily);
+                  ctx.textAlign = 'center';
+                  ctx.textBaseline = 'bottom';
+
+                  this.data.datasets.forEach(function (dataset, i) {
+                    var meta = chartInstance.controller.getDatasetMeta(i);
+
+                    meta.data.forEach(function (bar, index) {
+                      var data = dataset.data[index];
+                      ctx.fillText(data, bar._model.x, bar._model.y + 20);
+
+                    });
+
+                  });
+
+                }
+
+              }
            }
          })
 
@@ -86,21 +244,85 @@
         var perkemRealisExport = new Chart(perkemRealisExportCanvas,{
           type: 'bar',
           data: {
-          labels: ["a","b","c","d","e"],
+          labels: ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Des"],
           datasets: [
           {
             label: "Thousands (RP)",
-            backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
+            borderWidth: 1,
+            backgroundColor: "#878787",
             data: [200, 50, 30, 80, 70]
-          }
+          },
+          {
+            label: "Thousands (RP)",
+            borderWidth: 1,
+            backgroundColor: "#ff007f",
+            data: [200, 50, 30, 80, 70]
+          },
+          {
+            label: "Thousands (RP)",
+            borderWidth: 1,
+            backgroundColor: "#964b00",
+            data: [200, 50, 30, 80, 70]
+          },
         ]
           },
           options: {
+            responsive: true,
             legend: { display: false },
             title: {
               display: true,
-              
-            }
+            },
+             scales: {
+               yAxes: [{
+                 ticks: {
+                   beginAtZero: true
+                 }
+               }],
+               xAxes: [{
+                 gridLines: {
+                   display: false
+                 },
+                 ticks: {
+                   beginAtZero: true
+                 }
+               }]
+             },
+             "hover": {
+               "animationDuration": 0
+             },
+             onClick: function (event, array) {
+               let element = this.getElementAtEvent(event);
+               if (element.length > 0) {
+                 var series = element[0]._model.datasetLabel;
+                 var label = element[0]._model.label;
+                 var value = this.data.datasets[element[0]._datasetIndex].data[element[0]._index];
+                 alert(label + " dengan nilai " + value);
+               }
+             },
+             "animation": {
+               "duration": 1,
+               "onComplete": function () {
+                 var chartInstance = this.chart,
+                   ctx = chartInstance.ctx;
+
+                 ctx.font = Chart.helpers.fontString(Chart.defaults.global.defaultFontSize, Chart.defaults.global.defaultFontStyle, Chart.defaults.global.defaultFontFamily);
+                 ctx.textAlign = 'center';
+                 ctx.textBaseline = 'bottom';
+
+                 this.data.datasets.forEach(function (dataset, i) {
+                   var meta = chartInstance.controller.getDatasetMeta(i);
+
+                   meta.data.forEach(function (bar, index) {
+                     var data = dataset.data[index];
+                     ctx.fillText(data, bar._model.x, bar._model.y + 20);
+
+                   });
+
+                 });
+
+               }
+
+             }
           }
         })
       })
