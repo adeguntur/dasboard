2f75549b64f1c1259cfadd62f65ca72f6dd92666
ihalotec <ihalotec@gmail.com>
2020-06-02 16:46:50 +0700

    grafik - realisasi

--- controllers/C_Index.js
+++ controllers/C_Index.js
@@ -7,29 +7,29 @@ class C_Index {
 
     async chartApi(req, res){
       var pelabuhanKode = [];
-      var pelabuhanJmlpib = [];
+      var pelabuhanJmlpemasukan = [];
       var negaraKode = [];
       var negaraTotal = [];
       var importirNama = [];
       var importirTotal = [];
+      var border = [];
+      var realisasiBorder = [];
 
-      await db.any("SELECT kode_pelabuhan AS kode_pelabuhan, SUM(cif_rp) as total from(SELECT SUBSTRING(pelbkr, 3) AS kode_pelabuhan, (cif * ndpbm) AS cif_rp, pibtg FROM " +
-          "nswdb1.tblpibhdr WHERE pibtg BETWEEN '2019-01-01' AND '2019-12-31'" +
-          "GROUP BY pelbkr, cif_rp, pibtg) as foo" +
-          "group by kode_pelabuhan order by total desc limit 5;")
+      //pelabuhan
+      await db.any("SELECT kode_pelabuhan AS kode_pelabuhan, SUM(cif_rp) as total from (SELECT SUBSTRING(pelbkr, 3) AS kode_pelabuhan, (cif * ndpbm) AS cif_rp, pibtg FROM nswdb1.tblpibhdr WHERE pibtg BETWEEN '2019-01-01' AND '2019-12-31' GROUP BY pelbkr, cif_rp, pibtg) as foo group by kode_pelabuhan order by total desc limit 5;")
         .then((result) => {
-          console.log(result)
           for (let i = 0; i < result.length; i++) {
             pelabuhanKode.push(result[i].kode_pelabuhan);
-            pelabuhanJmlpib.push(result[i].total)
+            pelabuhanJmlpemasukan.push(result[i].total)
           }
-          
         })
         .catch((err) => {});
+
+      //negara impport
       await db.any("select pasokneg, SUM(cif_rp) as total from" +
-          " (SELECT pasokneg, (cif * ndpbm) AS cif_rp, pibtg FROM nswdb1.tblpibhdr " +
-          "WHERE pibtg BETWEEN '2019-01-01' AND '2019-12-31' GROUP BY pasokneg, cif_rp, pibtg) as foo" +
-          " group by pasokneg order by total desc limit 5;")
+      "(SELECT pasokneg, (cif * ndpbm) AS cif_rp, pibtg FROM nswdb1.tblpibhdr " +
+      "WHERE pibtg BETWEEN '2019-01-01' AND '2019-12-31' GROUP BY pasokneg, cif_rp, pibtg) as foo" +
+      " group by pasokneg order by total desc limit 5;")
         .then((result) => {
           for (let i = 0; i < result.length; i++) {
             negaraKode.push(result[i].pasokneg);
@@ -39,8 +39,10 @@ class C_Index {
         })
         .catch((err) => {});
 
-      await db.any('select impnama, SUM(cif_rp) as total from (SELECT impnama, (cif * ndpbm) AS cif_rp FROM nswdb1.tblpibhdr GROUP BY impnama,cif_rp) as foo group by impnama order by total desc limit 10;')
+      //importir
+      await db.any("select impnama, SUM(cif_rp) as total from (SELECT impnama, (cif * ndpbm) AS cif_rp FROM nswdb1.tblpibhdr GROUP BY impnama,cif_rp) as foo group by impnama order by total desc limit 10;")
         .then((result) => {
+          console.log(result)
           for (let i = 0; i < result.length; i++) {
             importirNama.push(result[i].impnama);
             importirTotal.push(result[i].total)
@@ -49,9 +51,26 @@ class C_Index {
         })
         .catch((err) => {})
 
+      //Border
+      await db.any(" SELECT date_part('month', a.pibtg) as bulan_pib, b.dcif, a.ndpbm"+
+      "FROM tblpibhdr a JOIN tblpibdtl b ON a.cusdecid = b.cusdecid JOIN tblctl_postborder c ON c.cusdecid = a.cusdecid"+
+      "JOIN tblrealisasi_postborder d ON d.seq = c.seq AND d.seri_brg = b.serial AND b.nohs::text = d.hs_code::text"+
+      "WHERE a.pibtg >= '2019-01-01'::date AND a.pibtg <= '2019-12-31'::date ")
+        .then((result) => {
+          console.log(result)
+          for (let i = 0; i < result.length; i++) {
+            bulan_pib_B.push(result[i].bulan_pib);
+            dcif_B.push(result[i].dcif);
+            ndpbm_B.push(result[i].ndpbm)
+          }
+        })
+        .catch((err) => {})
+
+
+
        res.send({
            kode_pelabuhan: pelabuhanKode,
-           jml_pib: pelabuhanJmlpib,
+           jml_pemasukan: pelabuhanJmlpemasukan,
            kode_negara: negaraKode,
            totalimport_negara: negaraTotal,
            nama_importir: importirNama,
--- public/js/demo_1/dashboard.js
+++ public/js/demo_1/dashboard.js
@@ -3,9 +3,9 @@ $(document).ready(function(){
         url: 'http://localhost:3000/api/grafik',
         method: 'GET',
         success: function(data) {
+            getPelabuhan('pelabuhan-pemasukan', data.kode_pelabuhan, data.jml_pemasukan);
             getNegaraimport('negara-import', data.kode_negara, data.totalimport_negara);
             getImportir('importir-terbesar', data.nama_importir, data.total_importir);
-            //getNegaraimport5('transactionsWeekly', data.data.transactions.transactionsWeekly);
             //getNegaraimport5('transactionsMonthly', data.data.transactions.transactionsMonthly);
         },
         error: function(data) {
@@ -15,6 +15,94 @@ $(document).ready(function(){
     
 });
 
+//grafik top 5 pelabuhan pendapatan
+async function getPelabuhan(chart, kode, total) {
+    var labels = [];
+    var jml_pemasukan = [];
+
+    for (var i in kode) {
+        labels.push(kode[i]);
+        jml_pemasukan.push(total[i]);
+    }
+
+    var chartData = {
+        labels: labels,
+        datasets: [{
+            label: "Thousands (RP)",
+            borderWidth: 1,
+            backgroundColor: ["#878787", "#ffa07a", "#ffda00", "#00ff5f", "#ff007f"],
+            fill: false,
+            data: jml_pemasukan
+        }]
+    };
+
+    var ctx = document.getElementById("pelabuhan-pemasukan")
+    var chart = new Chart(ctx, {
+        type: 'bar',
+        data: chartData,
+        options: {
+            responsive: true,
+            legend: {
+                display: false
+            },
+            title: {
+                display: true,
+            },
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }],
+                xAxes: [{
+                    gridLines: {
+                        display: false
+                    },
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            },
+            "hover": {
+                "animationDuration": 0
+            },
+            onClick: function (event, array) {
+                var span = document.getElementById("close");
+                let element = this.getElementAtEvent(event);
+                if (element.length > 0) {
+                    var series = element[0]._model.datasetLabel;
+                    var label = element[0]._model.label;
+                    var value = this.data.datasets[element[0]._datasetIndex].data[element[0]._index];
+                    alert("Kode negara " + label + " dengan nilai import thousands (RP) " + value);
+                }
+            },
+            "animation": {
+                "duration": 1,
+                "onComplete": function () {
+                    var chartInstance = this.chart,
+                        ctx = chartInstance.ctx;
+
+                    ctx.font = Chart.helpers.fontString(Chart.defaults.global.defaultFontSize, Chart.defaults.global.defaultFontStyle, Chart.defaults.global.defaultFontFamily);
+                    ctx.textAlign = 'center';
+                    ctx.textBaseline = 'bottom';
+
+                    this.data.datasets.forEach(function (dataset, i) {
+                        var meta = chartInstance.controller.getDatasetMeta(i);
+
+                        meta.data.forEach(function (bar, index) {
+                            var data = dataset.data[index];
+                            ctx.fillText(data, bar._model.x, bar._model.y + 20);
+
+                        });
+                    });
+
+                }
+
+            }
+        }
+    });
+}
+// grafik top 5 negara importir
 async function getNegaraimport(chart, kode, total) {
     var labels = [];
     var totalimportNegara = [];
@@ -101,7 +189,7 @@ async function getNegaraimport(chart, kode, total) {
         }
     });
 }
-
+//grafik top 10 importir  terbesar
 async function getImportir(chart, kode, total) {
     var labels = [];
     var total_importir = [];
