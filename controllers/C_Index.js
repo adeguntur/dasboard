54c7516c6ae1e39e823ca6d0b776be1c2495099c
ihalotec <ihalotec@gmail.com>
2020-06-02 12:15:30 +0700

    fix controler

--- controllers/C_Index.js
+++ controllers/C_Index.js
@@ -1,59 +1,54 @@
 const db = require('../config/database')
 
-var pelabuhanKode = [];
-var pelabuhanJmlpib = [];
-var negaraKode = [];
-var negaraTotal= [];
-var importirNama = [];
-var importirTotal = [];
-
 class C_Index {
    async index(req, res){
-  /*       if(req.isAuthenticated()){ */
-     await db.any("" +
-       +"SELECT SUBSTRING(pelbkr, 3) AS kode_pelabuhan, SUM(cif_rp) as total FROM" +
-       "(SELECT SUBSTRING(pelbkr, 3) AS kode_pelabuhan, (cif * ndpbm) as cif_rp, pibtg  FROM nswdb1.tblpibhdr " +
-       "WHERE pibtg BETWEEN '2019-01-01 ' AND '2019-12-31 ' GROUP BY kode_pelabuhan, cif, ndpbm, pibtg ) as foo" +
-       "GROUP BY kode_pelabuhan ORDER BY total DESC LIMIT 5 ;")
-            .then((result) => {
-              console.log(result)
-
-                for(let i = 0; i < result.length; i++){
-                    pelabuhanKode.push(result[i].kode_pelabuhan);
-                    pelabuhanJmlpib.push(result[i].total)
-                }
-                
-                res.render('index', { })
-            })
-            .catch((err) => {});
-            await db.any("select pasokneg, SUM(cif_rp) as total from (SELECT pasokneg, (cif * ndpbm) AS cif_rp, pibtg FROM nswdb1.tblpibhdr WHERE pibtg BETWEEN '2019-01-01' AND '2019-12-31' GROUP BY pasokneg, cif_rp, pibtg) as foo group by pasokneg order by total desc limit 10;")
-              .then((result) => {
-                for (let i = 0; i < result.length; i++) {
-                  negaraKode.push(result[i].pasokneg);
-                  negaraTotal.push(result[i].total);
-                }
-
-                res.render("index", {});
-              })
-              .catch((err) => {});
-          
-            await db.any('select impnama, SUM(cif_rp) as total from (SELECT impnama, (cif * ndpbm) AS cif_rp FROM nswdb1.tblpibhdr GROUP BY impnama,cif_rp) as foo group by impnama order by total desc limit 10;')
-                .then((result) => {
-                    for (let i = 0; i < result.length; i++) {
-                        importirNama.push(result[i].impnama);
-                        importirTotal.push(result[i].total)
-                    }
-
-                    res.render('index', {})
-                })
-                .catch((err) => {});
-            
-        /* }else{
-            res.redirect('/login')
-        } */
+      res.render('index', {})
     }
 
     async chartApi(req, res){
+      var pelabuhanKode = [];
+      var pelabuhanJmlpib = [];
+      var negaraKode = [];
+      var negaraTotal = [];
+      var importirNama = [];
+      var importirTotal = [];
+
+      await db.any("SELECT kode_pelabuhan AS kode_pelabuhan, SUM(cif_rp) as total from(SELECT SUBSTRING(pelbkr, 3) AS kode_pelabuhan, (cif * ndpbm) AS cif_rp, pibtg FROM " +
+          "nswdb1.tblpibhdr WHERE pibtg BETWEEN '2019-01-01' AND '2019-12-31'" +
+          "GROUP BY pelbkr, cif_rp, pibtg) as foo" +
+          "group by kode_pelabuhan order by total desc limit 5;")
+        .then((result) => {
+          console.log(result)
+          for (let i = 0; i < result.length; i++) {
+            pelabuhanKode.push(result[i].kode_pelabuhan);
+            pelabuhanJmlpib.push(result[i].total)
+          }
+          
+        })
+        .catch((err) => {});
+      await db.any("select pasokneg, SUM(cif_rp) as total from" +
+          " (SELECT pasokneg, (cif * ndpbm) AS cif_rp, pibtg FROM nswdb1.tblpibhdr " +
+          "WHERE pibtg BETWEEN '2019-01-01' AND '2019-12-31' GROUP BY pasokneg, cif_rp, pibtg) as foo" +
+          " group by pasokneg order by total desc limit 5;")
+        .then((result) => {
+          for (let i = 0; i < result.length; i++) {
+            negaraKode.push(result[i].pasokneg);
+            negaraTotal.push(result[i].total);
+          }
+          
+        })
+        .catch((err) => {});
+
+      await db.any('select impnama, SUM(cif_rp) as total from (SELECT impnama, (cif * ndpbm) AS cif_rp FROM nswdb1.tblpibhdr GROUP BY impnama,cif_rp) as foo group by impnama order by total desc limit 10;')
+        .then((result) => {
+          for (let i = 0; i < result.length; i++) {
+            importirNama.push(result[i].impnama);
+            importirTotal.push(result[i].total)
+          }
+       
+        })
+        .catch((err) => {})
+
        res.send({
            kode_pelabuhan: pelabuhanKode,
            jml_pib: pelabuhanJmlpib,
--- public/js/demo_1/dashboard.js
+++ public/js/demo_1/dashboard.js
@@ -1,336 +1,194 @@
-(function ($) {
-  'use strict';
-  $(function () {
-    //API URL
-    const url = 'http://localhost:3000/api/grafik'
-    fetch(url)
-      .then(res =>{
-        if (!res.ok) {
-          throw new Error(res.statusText);   
+$(document).ready(function(){
+    $.ajax({
+        url: 'http://localhost:3000/api/grafik',
+        method: 'GET',
+        success: function(data) {
+            getNegaraimport('negara-import', data.kode_negara, data.totalimport_negara);
+            getImportir('importir-terbesar', data.nama_importir, data.total_importir);
+            //getNegaraimport5('transactionsWeekly', data.data.transactions.transactionsWeekly);
+            //getNegaraimport5('transactionsMonthly', data.data.transactions.transactionsMonthly);
+        },
+        error: function(data) {
+            console.log(data);
         }
-        return res.json()
-      })
-      .then(
-        data => {
-        //5 Negara import
-        var modal = document.getElementById("myModal");
-        var negaraExportCanvas = document.getElementById("negara-export")
-        modal.style.display = "none"
-        var negaraExport = new Chart(negaraExportCanvas,{
-          type: 'bar',
-          data: {
-          labels: data.kode_negara,
-        datasets: [
-          {
+    });
+    
+});
+
+async function getNegaraimport(chart, kode, total) {
+    var labels = [];
+    var totalimportNegara = [];
+
+    for(var i in kode) {
+        labels.push(kode[i]);
+        totalimportNegara.push(total[i]);
+    }
+
+    var chartData = {
+        labels: labels,
+        datasets: [{
             label: "Thousands (RP)",
             borderWidth: 1,
             backgroundColor: ["#878787", "#ffa07a", "#ffda00", "#00ff5f", "#ff007f"],
-            data: data.totalimport_negara,
-          }
-          ]
-          },
-         options: {
-           responsive: true,
-           legend: {
-             display: false
-           },
-           title: {
-             display: true,
-           },
-           scales: {
-             yAxes: [{
-               ticks: {
-                 beginAtZero: true
-               }
-             }],
-             xAxes: [{
-               gridLines: {
-                 display: false
-               },
-               ticks: {
-                 beginAtZero: true
-               }
-             }]
-           },
-           "hover": {
-             "animationDuration": 0
-           },
-           onClick: function (event, array) {
-             var span = document.getElementById("close");
-             let element = this.getElementAtEvent(event);
-             if (element.length > 0) {
-               var series = element[0]._model.datasetLabel;
-               var label = element[0]._model.label;
-               var value = this.data.datasets[element[0]._datasetIndex].data[element[0]._index];
-               //alert("Kode negara " + label + " dengan nilai import thousands (RP) " + value);
-               modal.style.display = "block";
-           }
-          },
-           "animation": {
-             "duration": 1,
-             "onComplete": function () {
-               var chartInstance = this.chart,
-                 ctx = chartInstance.ctx;
+            fill: false,
+            data: totalimportNegara
+        }]
+    };
 
-               ctx.font = Chart.helpers.fontString(Chart.defaults.global.defaultFontSize, Chart.defaults.global.defaultFontStyle, Chart.defaults.global.defaultFontFamily);
-               ctx.textAlign = 'center';
-               ctx.textBaseline = 'bottom';
-
-               this.data.datasets.forEach(function (dataset, i) {
-                 var meta = chartInstance.controller.getDatasetMeta(i);
-
-                 meta.data.forEach(function (bar, index) {
-                   var data = dataset.data[index];
-                   ctx.fillText(data, bar._model.x, bar._model.y + 20);
-
-                 });
-               });
-
-             }
-
-           }
-         }
-        })
-        
-        //5 Pelabuhan pemasukan terbesar
-        var pelabuhanExportCanvas = document.getElementById("pelabuhan-pemasukan")
-        var pelabuhanExport = new Chart(pelabuhanExportCanvas,{
-          type: 'bar',
-          data: {
-          labels: data.kode_pelabuhan,
-          datasets: [
-          {
-            label: "Thousands (RP)",
-            borderWidth: 1,
-            backgroundColor: ["#878787", "#ff007f","#964b00","#ffda00","#ff7b00"],
-            data: data.jml_pib
-          }
-        ]
-          },
-          options: {
+    var ctx = document.getElementById("negara-import")
+    var chart = new Chart(ctx, {
+        type: 'bar',
+        data: chartData,
+        options: {
             responsive: true,
             legend: {
-              display: false,
-              text: ''
+                display: false
             },
             title: {
-              display: true,
+                display: true,
             },
-             scales: {
-               yAxes: [{
-                 ticks: {
-                   beginAtZero: true
-                 }
-               }],
-               xAxes: [{
-                 gridLines: {
-                   display: false
-                 },
-                 ticks: {
-                   beginAtZero: true
-                 }
-               }]
-             },
-             "hover": {
-               "animationDuration": 0
-             },
-             onClick: function (event, array) {
-               let element = this.getElementAtEvent(event);
-               if (element.length > 0) {
-                 var series = element[0]._model.datasetLabel;
-                 var label = element[0]._model.label;
-                 var value = this.data.datasets[element[0]._datasetIndex].data[element[0]._index];
-                 alert("Kode pelabuhan " + label + " dengan nilai pemasukan thousands (RP) " + value);
-               }
-             },
-             "animation": {
-               "duration": 1,
-               "onComplete": function () {
-                 var chartInstance = this.chart,
-                   ctx = chartInstance.ctx;
-
-                 ctx.font = Chart.helpers.fontString(Chart.defaults.global.defaultFontSize, Chart.defaults.global.defaultFontStyle, Chart.defaults.global.defaultFontFamily);
-                 ctx.textAlign = 'center';
-                 ctx.textBaseline = 'bottom';
-
-                 this.data.datasets.forEach(function (dataset, i) {
-                   var meta = chartInstance.controller.getDatasetMeta(i);
-
-                   meta.data.forEach(function (bar, index) {
-                     var data = dataset.data[index];
-                     ctx.fillText(data, bar._model.x, bar._model.y + 20);
-
-                   });
-
-                 });
-
-               }
-
-             }
-          }
-        })
-      
-         // 10 Importir terbesar
-         var importirExportCanvas = document.getElementById("importir-terbesar")
-         var importirExport = new Chart(importirExportCanvas, {
-           type: 'horizontalBar',
-           data: {
-             labels: data.nama_importir,
-             datasets: [{
-               label: "Thousands (RP)",
-               borderWidth: 1,
-               backgroundColor: "#878787",
-               data: data.total_importir
-             }]
-           },
-           options: {
-             responsive: true,
-             legend: {
-               display: false
-             },
-             title: {
-               display: true,
-             },
-              scales: {
+            scales: {
                 yAxes: [{
-                  ticks: {
-                    beginAtZero: true
-                  }
+                    ticks: {
+                        beginAtZero: true
+                    }
                 }],
                 xAxes: [{
-                  gridLines: {
-                    display: false
-                  },
-                  ticks: {
-                    beginAtZero: true
-                  }
+                    gridLines: {
+                        display: false
+                    },
+                    ticks: {
+                        beginAtZero: true
+                    }
                 }]
-              },
-              "hover": {
+            },
+            "hover": {
                 "animationDuration": 0
-              },
-              onClick: function (event, array) {
+            },
+            onClick: function (event, array) {
+                var span = document.getElementById("close");
                 let element = this.getElementAtEvent(event);
                 if (element.length > 0) {
-                  var series = element[0]._model.datasetLabel;
-                  var label = element[0]._model.label;
-                  var value = this.data.datasets[element[0]._datasetIndex].data[element[0]._index];
-                  alert("Nama importir " + label + " dengan nilai import thousands (RP)" + value);
+                    var series = element[0]._model.datasetLabel;
+                    var label = element[0]._model.label;
+                    var value = this.data.datasets[element[0]._datasetIndex].data[element[0]._index];
+                    alert("Kode negara " + label + " dengan nilai import thousands (RP) " + value);
                 }
-              },
-              "animation": {
+            },
+            "animation": {
                 "duration": 1,
                 "onComplete": function () {
-                  var chartInstance = this.chart,
-                    ctx = chartInstance.ctx;
+                    var chartInstance = this.chart,
+                        ctx = chartInstance.ctx;
 
-                  ctx.font = Chart.helpers.fontString(Chart.defaults.global.defaultFontSize, Chart.defaults.global.defaultFontStyle, Chart.defaults.global.defaultFontFamily);
-                  ctx.textAlign = 'center';
-                  ctx.textBaseline = 'bottom';
+                    ctx.font = Chart.helpers.fontString(Chart.defaults.global.defaultFontSize, Chart.defaults.global.defaultFontStyle, Chart.defaults.global.defaultFontFamily);
+                    ctx.textAlign = 'center';
+                    ctx.textBaseline = 'bottom';
 
-                  this.data.datasets.forEach(function (dataset, i) {
-                    var meta = chartInstance.controller.getDatasetMeta(i);
+                    this.data.datasets.forEach(function (dataset, i) {
+                        var meta = chartInstance.controller.getDatasetMeta(i);
 
-                    meta.data.forEach(function (bar, index) {
-                      var data = dataset.data[index];
-                      ctx.fillText(data, bar._model.x, bar._model.y + 20);
+                        meta.data.forEach(function (bar, index) {
+                            var data = dataset.data[index];
+                            ctx.fillText(data, bar._model.x, bar._model.y + 20);
 
+                        });
                     });
-
-                  });
 
                 }
 
-              }
-           }
-         })
-
-        //Perkembangan realisasi import 
-        var perkemRealisExportCanvas = document.getElementById("perkembangan-realisasi")
-        var perkemRealisExport = new Chart(perkemRealisExportCanvas,{
-          type: 'bar',
-          data: {
-          labels: ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Des"],
-          datasets: [
-          {
-            label: "Border",
-            borderWidth: 1,
-            backgroundColor: "#878787",
-            data: [200, 50, 30, 80, 70]
-          },
-          {
-            label: "Post Border",
-            borderWidth: 1,
-            backgroundColor: "#ff007f",
-            data: [200, 50, 30, 80, 70]
-          },
-          {
-            label: "Non Ijin",
-            borderWidth: 1,
-            backgroundColor: "#964b00",
-            data: [200, 50, 30, 80, 70]
-          },
-        ]
-          },
-          options: {
-            responsive: true,
-            legend: { display: false },
-            title: {
-              display: true,
-            },
-             scales: {
-               yAxes: [{
-                 ticks: {
-                   beginAtZero: true
-                 }
-               }],
-               xAxes: [{
-                 gridLines: {
-                   display: false
-                 },
-                 ticks: {
-                   beginAtZero: true
-                 }
-               }]
-             },
-             "hover": {
-               "animationDuration": 0
-             },
-             onClick: function (event, array) {
-               let element = this.getElementAtEvent(event);
-               if (element.length > 0) {
-                 var series = element[0]._model.datasetLabel;
-                 var label = element[0]._model.label;
-                 var value = this.data.datasets[element[0]._datasetIndex].data[element[0]._index];
-                 alert(series + " Bulan " + label + " dengan nilai thousands (RP). " + value );
-               }
-             },
-             "animation": {
-               "duration": 1,
-               "onComplete": function () {
-                 var chartInstance = this.chart,
-                   ctx = chartInstance.ctx;
-
-                 ctx.font = Chart.helpers.fontString(Chart.defaults.global.defaultFontSize, Chart.defaults.global.defaultFontStyle, Chart.defaults.global.defaultFontFamily);
-                 ctx.textAlign = 'center';
-                 ctx.textBaseline = 'bottom';
-
-                 this.data.datasets.forEach(function (dataset, i) {
-                   var meta = chartInstance.controller.getDatasetMeta(i);
-
-                   meta.data.forEach(function (bar, index) {
-                     var data = dataset.data[index];
-                     ctx.fillText(data, bar._model.x, bar._model.y + 20);
-
-                   });
-
-                 });
-
-               }
-
-             }
-          }
-        })
-      })
+            }
+        }
     });
-  })(jQuery);
+}
+
+async function getImportir(chart, kode, total) {
+    var labels = [];
+    var total_importir = [];
+
+    for (var i in kode) {
+        labels.push(kode[i]);
+        total_importir.push(total[i]);
+    }
+
+    var chartData = {
+        labels: labels,
+        datasets: [{
+            label: "Thousands (RP)",
+            borderWidth: 1,
+            backgroundColor: ["#878787", "#ffa07a", "#ffda00", "#00ff5f", "#ff007f"],
+            fill: false,
+            data: total
+        }]
+    };
+
+    var ctx = document.getElementById("importir-terbesar")
+    var chart = new Chart(ctx, {
+        type: 'horizontalBar',
+        data: chartData,
+        options: {
+            responsive: true,
+            legend: {
+                display: false
+            },
+            title: {
+                display: true,
+            },
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }],
+                xAxes: [{
+                    gridLines: {
+                        display: false
+                    },
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            },
+            "hover": {
+                "animationDuration": 0
+            },
+            onClick: function (event, array) {
+                var span = document.getElementById("close");
+                let element = this.getElementAtEvent(event);
+                if (element.length > 0) {
+                    var series = element[0]._model.datasetLabel;
+                    var label = element[0]._model.label;
+                    var value = this.data.datasets[element[0]._datasetIndex].data[element[0]._index];
+                    alert("Kode negara " + label + " dengan nilai import thousands (RP) " + value);
+                }
+            },
+            "animation": {
+                "duration": 1,
+                "onComplete": function () {
+                    var chartInstance = this.chart,
+                        ctx = chartInstance.ctx;
+
+                    ctx.font = Chart.helpers.fontString(Chart.defaults.global.defaultFontSize, Chart.defaults.global.defaultFontStyle, Chart.defaults.global.defaultFontFamily);
+                    ctx.textAlign = 'center';
+                    ctx.textBaseline = 'bottom';
+
+                    this.data.datasets.forEach(function (dataset, i) {
+                        var meta = chartInstance.controller.getDatasetMeta(i);
+
+                        meta.data.forEach(function (bar, index) {
+                            var data = dataset.data[index];
+                            ctx.fillText(data, bar._model.x, bar._model.y + 20);
+
+                        });
+                    });
+
+                }
+
+            }
+        }
+    });
+}
+
+function getPriceNumber(value){
+    return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+}
